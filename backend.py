925a5d19b4954fd06bc2f9595550b24217995271
yasminee <yasmincantikk3@gmail.com>
2026-06-01 19:42:30 +0700

    Refactor linked list and add binary search tree

--- backend.py
+++ backend.py
@@ -108,3 +108,105 @@ class queue:
 
     def size(self):
         return len(self.items)
+
+class TreeNode:
+    def __init__(self, buku, rating):
+        self.buku = buku
+        self.rating = int(rating)
+        self.left = None
+        self.right = None
+
+class bst:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, buku, rating):
+        self.root = self._insert_recursive(self.root, buku, int(rating))
+
+    def inorder(self, node, result):
+        if node:
+            self.inorder(node.left, result)
+            result.append((node.buku, node.rating))
+            self.inorder(node.right, result)
+
+    def _insert_recursive(self, node, buku, rating):
+        if not node:
+            return TreeNode(buku, rating)
+        if buku < node.buku:
+            node.left = self._insert_recursive(node.left, buku, rating)
+        elif buku > node.buku:
+            node.right = self._insert_recursive(node.right, buku, rating)
+        else:
+            node.rating = max(node.rating, rating)
+        return node
+
+    def search(self, buku):
+        return self._search_recursive(self.root, buku)
+
+    def _search_recursive(self, node, buku):
+        if not node or node.buku == buku:
+            return node
+        if buku < node.buku:
+            return self._search_recursive(node.left, buku)
+        return self._search_recursive(node.right, buku)
+
+    def get_books_by_min_rating(self, min_rating):
+        result = []
+        self._traverse_by_rating(self.root, result, min_rating)
+        return sorted(result, key=lambda x: x[1], reverse=True)
+
+    def _traverse_by_rating(self, node, result, min_rating):
+        if not node:
+            return
+        self._traverse_by_rating(node.left, result, min_rating)
+        if node.rating >= min_rating:
+            result.append((node.buku, node.rating))
+        self._traverse_by_rating(node.right, result, min_rating)
+
+    def get_books_by_rating_range(self, min_rating, max_rating):
+        result = []
+        self._traverse_range(self.root, result, min_rating, max_rating)
+        return sorted(result, key=lambda x: x[1], reverse=True)
+
+    def _traverse_range(self, node, result, min_rating, max_rating):
+        if not node:
+            return
+        self._traverse_range(node.left, result, min_rating, max_rating)
+        if min_rating <= node.rating <= max_rating:
+            result.append((node.buku, node.rating))
+        self._traverse_range(node.right, result, min_rating, max_rating)
+
+class bubble_sort:
+    @staticmethod
+    def sort(data):
+        n = len(data)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if data[j][2] < data[j + 1][2]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+        return data
+
+
+def bubble_sort_rating(data_list):
+    n = len(data_list)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if data_list[j][2] < data_list[j + 1][2]:
+                data_list[j], data_list[j + 1] = data_list[j + 1], data_list[j]
+    return data_list
+
+class RecommendationManager:
+    def __init__(self, bst_obj, linked_list_obj):
+        self.bst = bst_obj
+        self.linked_list = linked_list_obj
+
+    def get_top_rated_books(self, min_rating=4):
+        all_books = self.linked_list.get_unique_books_with_max_rating()
+        return [book for book in all_books if book[1] >= min_rating]
+
+    def get_books_in_rating_range(self, min_rating=3, max_rating=5):
+        return self.bst.get_books_by_rating_range(min_rating, max_rating)
+
+    def get_all_top_books_summary(self):
+        all_books = self.linked_list.get_unique_books_with_max_rating()
+        return [{"buku": buku, "rating": rating} for buku, rating in all_books]
